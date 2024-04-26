adb46d6fe52892a0ad022ff86dbdac4a12f0fdd7
Vladimir Smelov <vladimirfol@gmail.com>
2024-04-26 18:59:14 +0800

    args

--- examples/language/grok-1/run_inference_fast.sh
+++ examples/language/grok-1/run_inference_fast.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 torchrun --standalone --nproc_per_node 8 inference_tp.py --pretrained grok-1 \
-    --max_new_tokens 1000 \
+    --max_new_tokens 300 \
     --text "I will write you very detailed explanations of what is Ethereum and how it works. Ethereum is"
